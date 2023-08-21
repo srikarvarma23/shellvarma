180137bd87b287a0c46fb0e760d696e64909d50a
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-21 10:34:24 +0530

    committed

---
+++ 01variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "srikar: hi ra srinivas"
+echo "srinivas: hi ra mawa"
+echo "srikar: eroju plans em levu ga movie ki veldam"
+echo "srinivas: levu ra , em movie ki veldam"
+echo "srikar: saaho ki"
