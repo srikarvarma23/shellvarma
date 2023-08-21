9c7d481cd42cdf46c21406d51892f5e1eb73e9d5
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-21 10:53:38 +0530

    committed

---
+++ 03variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAME=$1
+NAME1=$2
+
+echo "$NAME: hi ra $NAME1"
+echo "$NAME1: hi ra $NAME"
+echo "$NAME: eroju plans em levu ga $NAME1 movie ki veldam"
+echo "$NAME1: levu ra $NAME , em movie ki veldam"
+echo "$NAME: saaho ki"
