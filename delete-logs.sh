7420c2f1bd4967597fefc93931dc12524ceaaf00
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-22 16:38:21 +0530

    newly committed

--- delete-logs.sh
+++ delete-logs.sh
@@ -9,3 +9,11 @@ LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+while read line 
+
+do 
+ echo ""deleting $line &>>$LOGFILE
+ rm -rf $line 
+
+done <<< $FILES_TO_DELETE
