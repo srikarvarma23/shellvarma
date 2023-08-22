af08ad41c90db1c28379366c3332e3f2f21d714f
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-22 15:27:38 +0530

    number committed

--- loops.sh
+++ loops.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellvarma-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
 
 R="\e[31m"
@@ -13,9 +16,25 @@ then
  exit 1 
 fi 
 
-
+VALIDATE(){
+ 
+ if [ $1 -ne 0 ]
+ then 
+  echo -e  " $R installating $2 failure $N"
+  exit 1
+  else 
+   echo -e " $G installating $2 success $N"
+   fi 
+}
 
 for i in $@
 do 
-   yum install $i -y 
+   yum list installed $i &>>$LOGFILE
+   if [ $? -ne 0 ]
+   then 
+    echo "if the package is not installed, lets install it"
+     yum install $i -y &>>$LOGFILE
+     VALIDATE $? "$i"
+    else 
+     echo -e " $Y $i is already installed, skip $N"
 done 
