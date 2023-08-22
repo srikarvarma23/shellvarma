9587e8cd4af4c8d01f2d34984f3605c1b2ff9234
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-22 15:04:50 +0530

    number committed

--- loops.sh
+++ loops.sh
@@ -2,9 +2,14 @@
 
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $USERID -ne 0 ];
 then 
- echo "error: please do it with root access"
+ echo "error: $R please do it with root access $N"
  exit 1 
 fi 
 
