170a3e6ae3049ff54baa6ec4e5640760d4793e60
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-22 16:25:39 +0530

    number committed

--- delete-logs.sh
+++ delete-logs.sh
@@ -6,6 +6,6 @@ SCRIPT_NAME=$0
 LOGSDIR=/home/centos/shellvarma-logs
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
-FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" - type f -mtime +14)
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
