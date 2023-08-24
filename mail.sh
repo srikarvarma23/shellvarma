061c4f9327b2d15c7a7a3b45ae8ff12d5a540ba9
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-24 09:46:58 +0530

    script added

--- mail.sh
+++ mail.sh
@@ -8,6 +8,6 @@ ALERT_TYPE=$5
 
 #echo "all args: $@"
 
-FINAL_BODY=$(sed -e 's/Team-Name/DevOps/g' -e 's/ALERT_TYPE/high disk usage/g' -e "s/MESSAGE/$message/") template.html
+FINAL_BODY=$(sed -e 's/Team-Name/DevOps/g' -e 's/ALERT_TYPE/high disk usage/g' -e "s/MESSAGE/$BODY/g" template.html)
 
-echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
+echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
