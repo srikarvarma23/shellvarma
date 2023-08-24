4bab1a44b7895418cda61ad1e9cf5d5a1b45c831
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-24 09:42:42 +0530

    script added

--- mail.sh
+++ mail.sh
@@ -6,4 +6,8 @@ BODY=$3
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-echo "all args: $@"
+#echo "all args: $@"
+
+FINAL_BODY=$(sed -e 's/Team-Name/DevOps/g' -e 's/ALERT_TYPE/high disk usage/g' -e "s/MESSAGE/$message/") template.html
+
+echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
