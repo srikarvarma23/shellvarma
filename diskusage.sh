258d4a9b234ef8152306c688f70b9139da0d6cdf
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-24 08:19:27 +0530

    script added

---
+++ diskusage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LOGSDIR=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USAGE_THRESHOLD=0.5
+
+
+while IFS read line 
+do 
+
+ echo "output: $line"
+ 
+done <<< $DISK_USAGE
