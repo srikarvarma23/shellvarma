9a4e07ad1d8925f2b67aec412f17b0581051faa9
Srikar Gottumukkala <srikar.gottumukkala@ibm.com>
2023-08-24 08:39:54 +0530

    script added

--- diskusage.sh
+++ diskusage.sh
@@ -20,7 +20,7 @@ do
 usage=$(echo $line | awk '{print $6}' | cut -d % -f 1)
 partition=$(echo $line | awk '{print $1}')
 
-if [ $usage gt $DISK_USAGE_THRESHOLD ]
+if [ $usage -gt $DISK_USAGE_THRESHOLD ]
 then 
   message+=" $R it is running out of disk storage $N"
  fi 
